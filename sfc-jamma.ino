1a76aa94a035e869a8fc2257d4e0662ab0853820
Vincent Milum Jr <git@darkain.com>
2023-05-04 17:00:26 -0700

    SCF to JAMMA v1.0

--- sfc-jamma.ino
+++ sfc-jamma.ino
@@ -3,40 +3,65 @@
 #define DATA_LATCH    16
 #define BUTTON_MAX    16
 
-#define P1_COIN       11
-#define P1_START      10
+#define PROFILE_MAX    2
 
-#define P1_UP          3
-#define P1_DOWN        4
-#define P1_LEFT        5
-#define P1_RIGHT       6
+#define P1_COIN        3
+#define P1_START       4
+#define P1_TEST        0
 
-#define P1_LP          7
-#define P1_MP          8
-#define P1_HP          9
+#define P1_UP          5
+#define P1_DOWN        6
+#define P1_LEFT        7
+#define P1_RIGHT       8
+
+#define P1_LP          9
+#define P1_MP         10
+#define P1_HP         11
 
 #define P1_LK         17
 #define P1_MK         18
 #define P1_HK         19
 
 
-int P1[BUTTON_MAX] = {
-  P1_LK,
-  P1_LP,
-  P1_COIN,
-  P1_START,
-  P1_UP,
-  P1_DOWN,
-  P1_LEFT,
-  P1_RIGHT,
-  P1_MK,
-  P1_MP,
-  P1_HP,
-  P1_HK,
-  0xFF,
-  0xFF,
-  0xFF,
-  0xFF,
+int profile = 0;
+
+
+int P1[PROFILE_MAX][BUTTON_MAX] = {
+  {
+    P1_LK,
+    P1_LP,
+    P1_COIN,
+    P1_START,
+    P1_UP,
+    P1_DOWN,
+    P1_LEFT,
+    P1_RIGHT,
+    P1_MK,
+    P1_MP,
+    P1_HP,
+    P1_HK,
+    0xFF,
+    0xFF,
+    0xFF,
+    0xFF,
+  }, {
+    P1_LK,
+    P1_LP,
+    P1_COIN,
+    P1_START,
+    P1_UP,
+    P1_DOWN,
+    P1_LEFT,
+    P1_RIGHT,
+    P1_HK,
+    P1_HP,
+    P1_MP,
+    P1_MK,
+    0xFF,
+    0xFF,
+    0xFF,
+    0xFF,
+  }
 };
 
 
@@ -47,8 +72,8 @@ void setup() {
 
   // SET ALL JAMMA PINS TO "INPUT" MODE
   for (int i=0; i<BUTTON_MAX; i++) {
-    if (P1[i] == 0xFF) continue;
-    pinMode(P1[i], INPUT);
+    if (P1[profile][i] == 0xFF) continue;
+    pinMode(P1[profile][i], INPUT);
   }
   
   // Set DATA_CLOCK normally HIGH
@@ -90,18 +115,26 @@ void loop() {
   // READ THE CURRENT BUTTON STATE
   unsigned int buttons = read_button();
 
+
+  // CONTROLLER APPEARS UNPLUGGED
+  // PROCESS AS NO BUTTONS PRESSED INSTEAD
+  if (buttons == 0x0000) buttons = 0xFFFF;
+  
+
   // LIGHT UP LED IF ANY BUTTONS ARE PRESSED
-  digitalWrite(LED_BUILTIN, (buttons != 0xffff));
+  digitalWrite(LED_BUILTIN, (buttons != 0xFFFF));
 
 
   // SEND BUTTON DATA TO JAMMA
   for (int i=0; i<BUTTON_MAX; i++) {
-    if (P1[i] == 0xFF) continue;
+    int btn = P1[profile][i];
+    if (btn == 0xFF) continue;
+    
     if (buttons & (0x01 << i)) {
-      pinMode(P1[i], INPUT);
+      pinMode(btn, INPUT);
     } else {
-      pinMode(P1[i], OUTPUT);
-      digitalWrite(P1[i], LOW);
+      pinMode(btn, OUTPUT);
+      digitalWrite(btn, LOW);
     }
   }
 
